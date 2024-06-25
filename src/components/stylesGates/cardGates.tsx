ede6f4df4d48114880a94c0aaeaf98d1837327a5
Marcos Vallejo <47191638+Markvallejo@users.noreply.github.com>
2024-06-25 16:45:29 -0600

    add gallery for styles gates grid

--- src/components/stylesGates/cardGates.tsx
+++ src/components/stylesGates/cardGates.tsx
@@ -1,29 +1,48 @@
 import "@/styles/stylesGates/cardGates.css";
+import { useState } from "react";
+import Modal from "../modal/modal";
+import Gallery from "../gallery/gallery";
 
-interface CardGatesProps { 
+interface CardGatesProps {
   image: string;
   title: string;
-  description: string
+  description: string;
   currentIndex: number;
 }
 
-const CardGates = ({image, title, description, currentIndex } : CardGatesProps ) => {
+const CardGates = ({
+  image,
+  title,
+  description,
+  currentIndex,
+}: CardGatesProps) => {
   const classNameId = "card-gates";
+  const [modalOpen, setModalOpen] = useState(false);
 
+  const closeModal = () => {
+    setModalOpen(false);
+  };
   const handleClick = () => {
-    console.log("CardGates clicked: ", currentIndex);
-  }
+    setModalOpen(true);
+  };
 
   return (
-    <div className={`${classNameId}`} onClick={handleClick} >
-      <div className={`${classNameId}__image-container`} > 
-        <img src={image} alt="style gate" />
+    <>
+      <div className={`${classNameId}`} onClick={handleClick}>
+        <div className={`${classNameId}__image-container`}>
+          <img src={image} alt="style gate" />
+        </div>
+        <div className={`${classNameId}__text-container`}>
+          <h3>{title}</h3>
+          <div dangerouslySetInnerHTML={{ __html: description }} />
+        </div>
       </div>
-      <div className={`${classNameId}__text-container`}>
-        <h3>{title}</h3>
-        <div dangerouslySetInnerHTML={{__html: description}} />
-      </div>
-    </div>  
+      {
+        modalOpen && <Modal title="Menu" closeModalHandler={closeModal} >
+          <Gallery currentIndex={currentIndex} />
+        </Modal>
+      }
+    </>
   );
 };
 
