51c96b4bb502681dfa7c809c51f69edb8d1188eb
Marcos Vallejo <47191638+Markvallejo@users.noreply.github.com>
2024-06-24 22:07:40 -0600

    create modal component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,29 +1,44 @@
+import { useState } from "react";
 import "@/styles/header/header.css";
+import Modal from "../modal/modal";
 
 const Header = () => {
+
+  const [modalOpen, setModalOpen] = useState(false);
   const handleMenu = () => {
+    setModalOpen(true);
     console.log("open menu");
+  };
+  const closeModal = () => {
+    setModalOpen(false);
   };
 
   return (
-    <div className="header-wrapper">
-      <div className="header-container">
-        <div className="text-container">
-          <h1 className="header">Affordable Automatic Gate</h1>
-          <p>-Houston valley-</p>
-        </div>
-        <div className="hamburger-button--container">
-          <button onClick={handleMenu}>
-            <img
-              src="/assets/header/header_menu.png"
-              alt="icon-menu"
-              height={49}
-              width={56}
-            />
-          </button>
+    <>
+      <div className="header-wrapper">
+        <div className="header-container">
+          <div className="text-container">
+            <h1 className="header">Affordable Automatic Gate</h1>
+            <p>-Houston valley-</p>
+          </div>
+          <div className="hamburger-button--container">
+            <button onClick={handleMenu}>
+              <img
+                src="/assets/header/header_menu.png"
+                alt="icon-menu"
+                height={49}
+                width={56}
+                />
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+      {
+        modalOpen && <Modal closeModalHandler={closeModal} >
+          <div>This is Modal</div>
+        </Modal>
+      }
+    </>
   );
 };
 
---
+++ src/components/modal/modal.tsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react'
+import ReactDOM from 'react-dom'
+
+import "@/styles/modal/modal.css";
+
+interface ModalProps {
+  closeModalHandler: () => void
+  children: React.ReactNode
+}
+
+const Modal = ({ children, closeModalHandler }: ModalProps ) => {
+  const [modalRoot, setModalRoot] = useState<HTMLElement | null>(null)
+  const classNameId = 'modal'
+
+  useEffect(() => {
+    const body = document.querySelector('body');
+    body?.classList.add('body-hidden');
+    const modal = document.getElementById('modal-root');
+    setModalRoot(modal);
+
+    return () => {
+      body?.classList.remove('body-hidden');
+    };
+  }, []);
+
+  return modalRoot
+    ? ReactDOM.createPortal(
+      <div className={`${classNameId}`}>
+        <div className={`${classNameId}__overlay`} />
+        {/* { showCloseButton && <ButtonClose onClick={closeModalHandler} /> } */}
+        <div className={`${classNameId}__content`}>
+          {children}
+        </div>
+      </div>,
+      modalRoot
+      )
+    : null
+}
+
+export default Modal
