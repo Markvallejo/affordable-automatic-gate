cc0f97f1c905ee96bf5ed8462c797c116fd0eed8
Marcos Vallejo <47191638+Markvallejo@users.noreply.github.com>
2024-09-24 23:55:07 -0600

    udpate meta tags for SEO

--- src/helpers/AppConfig.ts
+++ src/helpers/AppConfig.ts
@@ -1,8 +1,9 @@
 export const AppConfig = {
-  site_name: 'Affordable Automatic Gate',
-  title: 'Affordable Automatic Gate',
-  description: 'Affordable Automatic Gate.',
-  author: 'Affordable Automatic Gate',
+  site_name: 'Affordable Automatic Gate Houston',
+  title: 'Affordable Automatic Gate Houston',
+  description: 'Affordable Automatic Gate Houston, affordable and secure electric gate sales, installation and repair for homes and businesses. Contact us for a quick quote.',
+  author: 'Automatic gates Houston',
   locale_region: 'US',
   locale: 'en_US',
+  url: 'https://www.affordableautomaticgatehouston.com/'
 };
